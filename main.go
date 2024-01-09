aab211cb12aecaa882ca6f5eee58bb3cb6885499
m20129 <m20129@g.metro-cit.ac.jp>
2024-01-09 13:55:50 +0900

    first commit

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4436", "message": "こんにちは"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4436", "message": "Hello"})
     }
 }
